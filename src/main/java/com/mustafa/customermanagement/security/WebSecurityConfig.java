f36c11c41fadd3a5aad980a3f1fffd772fc2eb76
MustafaAliBaig <mirzabaig.mustafaali@gmail.com>
2025-06-12 21:04:49 -0700

    Add a simple in-memory authentication (for now, to be replaced with a database later).

---
+++ src/main/java/com/mustafa/customermanagement/security/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.mustafa.customermanagement.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
