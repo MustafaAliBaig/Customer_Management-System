b3c91dda24395bf653152d6ee485874cfa00f0b6
MustafaAliBaig <mirzabaig.mustafaali@gmail.com>
2025-06-18 15:45:04 -0700

    Commented WebSecurityConfig File

--- src/main/java/com/mustafa/customermanagement/security/WebSecurityConfig.java
+++ src/main/java/com/mustafa/customermanagement/security/WebSecurityConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.mustafa.customermanagement.security;
 
 import org.springframework.context.annotation.Configuration;
@@ -15,3 +16,4 @@ public class WebSecurityConfig {
     }
 
 }
+*/
